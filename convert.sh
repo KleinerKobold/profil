8bfb20cd7ab3f722c9c5f4f27a1d5f4199ed8ac7
oli <oli@clarissa.(none)>
2014-02-27 16:45:45 +0100

    Init

---
+++ convert.sh
@@ -0,0 +1,4 @@
+java -classpath ~/xmlcalabash-1.0.16-94/calabash.jar:/usr/share/fop/fop-hyph.jar:/usr/share/java/fop-1.0.jar:/usr/share/java/fop.jar:/usr/share/maven-repo/org/apache/xmlgraphics/fop/1.0/fop-1.0.jar:/usr/share/maven-repo/org/apache/xmlgraphics/fop/debian/fop-debian.jar:/usr/share/java/jeuclid-fop-3.1.9.jar:/usr/share/java/jeuclid-fop.jar:/usr/share/java/jlatexmath-fop-0.9.7.jar:/usr/share/java/jlatexmath-fop.jar:/usr/share/java/jlatexmath-plugin-fop.jar com.xmlcalabash.drivers.Main transform.xpl
+
+
+
