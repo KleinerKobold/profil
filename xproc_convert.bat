206d9d53c8a66e37a86e87ae5b914036e876e578
Oliver <oliver.r.staudt@gmail.com>
2014-03-09 06:22:37 +0100

    Verbesserungen der Batch

--- xproc_convert.bat
+++ xproc_convert.bat
@@ -1,17 +1,18 @@
 @echo off
 SETLOCAL ENABLEDELAYEDEXPANSION
-del Profile.pdf
+
+IF EXIST Profile.pdf del Profile.pdf
 
 REM Finde alle jars für den classpath
-set FOPDIR=fop-1.1-bin
+set FOPDIR=fop-1.1
 set CALABASHDIR=calabash-1.0.16-94
 set cp=
-for /r %FOPDIR% %%i in (build\*.jar lib\*.jar) do set cp=!cp!;%%i
-for /r %CALABASHDIR% %%i in (*.jar) do set cp=!cp!;%%i
+for /r %FOPDIR% %%i in (build\*.jar lib\*.jar) do set cp=!cp!;"%%i"
+for /r %CALABASHDIR% %%i in (*.jar) do set cp=!cp!;"%%i"
 
 java -classpath %cp% com.xmlcalabash.drivers.Main transform.xpl
 
-Profile.pdf
+IF EXIST Profile.pdf Profile.pdf
 
 
 ENDLOCAL
